23e366e9316d58cf6cd01c1b1406af2beea06074
sleeping-lion <toughjjh@gmail.com>
2017-01-17 04:01:13 +0900

    update

--- db/migrate/20121223140414_create_impressions_table.rb
+++ db/migrate/20121223140414_create_impressions_table.rb
@@ -1,19 +1,22 @@
 class CreateImpressionsTable < ActiveRecord::Migration
   def self.up
     create_table :impressions, :force => true do |t|
-      t.string :impressionable_type, :limit=>100
+      t.string :impressionable_type
       t.integer :impressionable_id
       t.integer :user_id
-      t.string :controller_name, :limit=>60
-      t.string :action_name, :limit=>60
-      t.string :view_name, :limit=>60
-      t.string :request_hash, :limit=>130
-      t.string :ip_address, :limit=>60
-      t.string :session_hash, :limit=>100
+      t.string :controller_name
+      t.string :action_name
+      t.string :view_name
+      t.string :request_hash
+      t.string :ip_address
+      t.string :session_hash
       t.text :message
       t.text :referrer
+      t.text :params      
       t.timestamps :null=>false
     end
+    
+    add_index :impressions, [:impressionable_type, :impressionable_id, :params], :name => "poly_params_request_index", :unique => false    
     add_index :impressions, [:impressionable_type, :impressionable_id, :request_hash], :name => "poly_request_index", :unique => false
     add_index :impressions, [:impressionable_type, :impressionable_id, :ip_address], :name => "poly_ip_index", :unique => false
     add_index :impressions, [:impressionable_type, :impressionable_id, :session_hash], :name => "poly_session_index", :unique => false
