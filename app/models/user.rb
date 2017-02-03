4db7fb3866356841cf10fec362c968654b34ab1c
sleepinglion <admin@sleepinglion.pe.kr>
2017-02-03 14:20:50 +0900

    수정함:        Gemfile.lock 	수정함:        app/models/user.rb 	수정함:        db/seeds.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ require 'carrierwave/orm/activerecord'
 
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatables
-  validates_presence_of :name 
+  validates_presence_of :nickname 
   validates_uniqueness_of :email
   belongs_to :group, :autosave=>true
-  mount_uploader :photo, UserUploader  
+  mount_uploader :photo, UserUploader
   has_many :user_photo, :dependent=> :destroy
   has_many :notice, :dependent=> :destroy
   has_many :blog, :dependent=> :destroy
