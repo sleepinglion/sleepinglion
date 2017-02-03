3a123bf351be41759da4c8f7c0ff74fdd34e0f13
sleepinglion <admin@sleepinglion.pe.kr>
2017-02-03 14:02:51 +0900

    수정함:        config/initializers/carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
 #      :region                 => 'ap-northeast-1'                  # optional, defaults to 'us-east-1'
       #:host                   => 's3.example.com',             # optional, defaults to nil
       #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-          }
+#          }
 #    config.fog_directory  = 'slhome'                     # require
 #config.fog_public     = false                                   # optional, defaults to true
 #    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
