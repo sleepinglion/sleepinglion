5354a5376c76b55279d9723d11aef9d222842252
sleeping-lion <toughjjh@gmail.com>
2017-02-05 02:54:42 +0900

    update

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -41,6 +41,12 @@ SitemapGenerator::Sitemap.create do
   GuestBook.find_each do |guest_book|
     add guest_book_path(guest_book), :lastmod => guest_book.updated_at
   end
+  
+  add programs_path
+
+  Program.find_each do |program|
+    add program_path(program), :lastmod => program.updated_at
+  end  
 
   add notices_path
 
