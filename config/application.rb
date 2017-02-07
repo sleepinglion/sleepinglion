ccb592671bfd3fb0431ab1df20bcc3d83ae4bc60
sleepinglion <admin@sleepinglion.pe.kr>
2017-02-07 18:40:22 +0900

    수정함:        Gemfile 	수정함:        config/application.rb 	수정함:        config/deploy/staging.rb

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,13 @@ module Sleepinglion
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Seoul'
-    
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ko
     I18n.enforce_available_locales=true
-    #config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 10.days} 
-    #config.autoload_paths += %W(#{config.root}/app/models/ckeditor)       
+    #config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 10.days}
+    #config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")    
   end
 end
