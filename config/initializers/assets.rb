2e0e9b5b684af91946ce566b30413d63f633a5cc
sleepinglion <admin@sleepinglion.pe.kr>
2017-02-07 17:18:47 +0900

    수정함:        config/initializers/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,20 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( ie.css jquery.js bootstrap.min.js home.js )
+js_prefix    = 'app/assets/javascripts/'
+style_prefix = 'app/assets/stylesheets/'
+image_prefix    = 'app/assets/images/'
+
+javascripts = Dir["#{js_prefix}**/*.js"].map      { |x| x.gsub(js_prefix,    '') }
+css         = Dir["#{style_prefix}**/*.css"].map  { |x| x.gsub(style_prefix, '') }
+image       = Dir["#{image_prefix}**/*"].map  { |x| x.gsub(image_prefix, '') }
+scss        = Dir["#{style_prefix}**/*.scss"].map { |x| x.gsub(style_prefix, '') }
+coffee        = Dir["#{js_prefix}**/*.coffee"].map { |x| x.gsub(js_prefix, '') }
+
+Rails.application.config.assets.precompile = (javascripts + css + scss + coffee + image)
+Rails.application.config.assets.precompile += Ckeditor.assets
+Rails.application.config.assets.precompile += %w( ckeditor/* )
+Rails.application.config.assets.precompile += %w( admin/new.js,ajax-loader.gif )
 Rails.application.config.assets.precompile << Proc.new { |path|
   if path =~ /\.(eot|svg|ttf|woff)\z/
     true
