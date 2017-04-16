85e5f6b4bc920921824575ba24f1e3aabae611a5
sleeping-lion <toughjjh@gmail.com>
2017-04-17 01:26:33 +0900

    새 파일:       db/migrate/20170416133221_translate_blog_categories.rb 	새 파일:       db/migrate/20170416133222_translate_blogs.rb 	새 파일:       db/migrate/20170416133223_translate_gallery_categories.rb 	새 파일:       db/migrate/20170416133224_translate_galleries.rb

---
+++ db/migrate/20170416133221_translate_blog_categories.rb
@@ -0,0 +1,14 @@
+class TranslateBlogCategories < ActiveRecord::Migration
+    def change
+        reversible do |dir|
+            dir.up do
+                BlogCategory.create_translation_table!({ title: :string}, migrate_data: true)
+                remove_column :blog_categories, :title
+            end
+
+            dir.down do
+                BlogCategory.drop_translation_table! migrate_data: true
+            end
+        end
+    end
+end
---
+++ db/migrate/20170416133223_translate_gallery_categories.rb
@@ -0,0 +1,14 @@
+class TranslateGalleryCategories < ActiveRecord::Migration
+    def change
+        reversible do |dir|
+            dir.up do
+                GalleryCategory.create_translation_table!({ title: :string}, migrate_data: true)
+                remove_column :gallery_categories, :title
+            end
+
+            dir.down do
+                GalleryCategory.drop_translation_table! migrate_data: true
+            end
+        end
+    end
+end
