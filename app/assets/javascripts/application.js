3afa6170ef0617c10ae15b46dab17c31f7e44a84
sleepinglion <toughjjh@gmail.com>
2017-06-12 23:38:13 +0900

    수정함:        app/assets/javascripts/application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -76,4 +76,14 @@ $(document).ready(function(){
 			$(this).find('span:eq(1)').html('&nbsp;&gt;&gt;');
 		}
 	});
+
+	$("a.simple_image").fancybox({
+			'opacity'   : true,
+			'overlayShow'        : true,
+			'overlayColor': '#000000',
+			'overlayOpacity'     : 0.9,
+			'titleShow':true,
+			'openEffect'  : 'elastic',
+			'closeEffect' : 'elastic'
+		});	
 });
