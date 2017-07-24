1d14995c266478ad7ced55f357858fabb8d6de14
sleepinglion <toughjjh@gmail.com>
2017-07-24 22:29:11 +0900

    수정함:        config/initializers/carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :ftp
-    config.ftp_host = "sleepinglion.cdn3.cafe24.com"
+    config.ftp_host = "slboard.cdn3.cafe24.com"
     config.ftp_port = 21
-    config.ftp_user = "sleepinglion"
+    config.ftp_user = "slboard"
     config.ftp_passwd = "jjh30750"
     config.ftp_folder = "/www"
-    config.ftp_url = "http://sleepinglion.cdn3.cafe24.com"
+    config.ftp_url = "http://slboard.cdn3.cafe24.com"
     config.ftp_passive = true
   else
     config.storage = :file
