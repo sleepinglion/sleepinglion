cf706551cf8c08a50cd662019eea45ae94c266ab
sleepinglion <admin@sleepinglion.pe.kr>
2021-02-10 03:40:48 +0900

    수정함:        config/initializers/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,17 +8,16 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-js_prefix    = 'app/assets/javascripts/'
+#js_prefix    = 'app/assets/javascripts/'
 style_prefix = 'app/assets/stylesheets/'
 image_prefix    = 'app/assets/images/'
 
-javascripts = Dir["#{js_prefix}**/*.js"].map      { |x| x.gsub(js_prefix,    '') }
+#javascripts = Dir["#{js_prefix}**/*.js"].map      { |x| x.gsub(js_prefix,    '') }
 css         = Dir["#{style_prefix}**/*.css"].map  { |x| x.gsub(style_prefix, '') }
 image       = Dir["#{image_prefix}**/*"].map  { |x| x.gsub(image_prefix, '') }
 scss        = Dir["#{style_prefix}**/*.scss"].map { |x| x.gsub(style_prefix, '') }
 
-Rails.application.config.assets.precompile = (javascripts + css + scss + image)
-Rails.application.config.assets.precompile += %w( new.js )
+Rails.application.config.assets.precompile = (css + scss + image)
 Rails.application.config.assets.precompile << Proc.new { |path|
   if path =~ /\.(eot|svg|ttf|woff|woff2)\z/
     true
